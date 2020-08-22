d1fc0553d80695339ae5148fbbcf92402a5c79e8
antkumar73 <antlumar73@gmail.com>
2020-08-22 12:39:45 +0530

    dev branch hello world change

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,16 @@
-print("Annaanan")
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:\Users\Anant Kumar\OneDrive\Desktop\LearnGitRepo\hello_world.py ==
+Annaanan
+Anant ki jai
+Anant ki jai
+Anant ki jai
+Anant ki jai
+Anant ki jai
+Anant ki jai
+Anant ki jai
+Anant ki jai
+Anant ki jai
+Anant ki jai
+>>> 
