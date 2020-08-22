3b95f19dbbd78dc96e48390aafb967f2f9b3e1db
antkumar73 <antlumar73@gmail.com>
2020-08-22 12:16:25 +0530

    for commit

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> for i in range(10):
+	print("yahooo")
+
+yahooo
+yahooo
+yahooo
+yahooo
+yahooo
+yahooo
+yahooo
+yahooo
+yahooo
+yahooo
+>>> 
